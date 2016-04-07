29b5fea554fdf44fea27aec6fa36b854f1f2631f
David Zhang <david-zhang0501@hotmail.com>
2016-04-06 23:33:26 -0700

    added drag and click func

--- app/src/main/java/com/example/anson/bustexter2/TopButton.java
+++ app/src/main/java/com/example/anson/bustexter2/TopButton.java
@@ -7,21 +7,26 @@ import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.IBinder;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.view.GestureDetector;
 
 
 /**
  * Created by Anson on 2016-03-27.
  */
-public class TopButton extends Service{
+public class TopButton extends Service {
     private WindowManager windowManager;
     private ImageView chatHead;
+    WindowManager.LayoutParams params;
+    private GestureDetector gestureDetector;
 
     String textTransLink = "33333";
 
@@ -30,45 +35,40 @@ public class TopButton extends Service{
     String oak41 = "50805";
     String hospital = "51479";
 
-    //magic that removes an error, don't touch
-    @Override public IBinder onBind(Intent intent) {
-        // Not used
-        return null;
-    }
 
-    @Override public void onCreate() {
+    @Override
+    public void onCreate() {
         super.onCreate();
 
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        gestureDetector = new GestureDetector(this, new SingleTapConfirm());
 
         chatHead = new ImageView(this);
         chatHead.setImageResource(R.mipmap.ic_launcher);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_PHONE,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
 
-        params.gravity = Gravity.CENTER | Gravity.BOTTOM;
+        params.gravity = Gravity.TOP | Gravity.LEFT;
         params.x = 0;
         params.y = 0;
 
-        //texts TransLink, right now only the 49 from UBC. update to other common bus numbers
+        /*
         chatHead.setOnClickListener(new View.OnClickListener() {
             @TargetApi(Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View view) {
 
-                /** Instantiating PopupMenu class */
                 PopupMenu popup = new PopupMenu(getBaseContext(), view,
                         Gravity.START | Gravity.CENTER_HORIZONTAL);
 
-                /** Adding menu items to the popumenu */
                 popup.getMenuInflater().inflate(R.menu.menu_main, popup.getMenu());
 
-                /** Defining menu item click listener for the popup menu */
+
                 popup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
 
                                                      //@Override
@@ -100,8 +100,83 @@ public class TopButton extends Service{
 
                 );
 
-                /** Showing the popup menu */
                 popup.show();
+            }
+        });
+*/
+        chatHead.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (gestureDetector.onTouchEvent(event)) {
+                    PopupMenu popup = new PopupMenu(getBaseContext(), v,
+                            Gravity.START | Gravity.CENTER_HORIZONTAL);
+
+                    popup.getMenuInflater().inflate(R.menu.menu_main, popup.getMenu());
+
+
+                    popup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+                                                         //@Override
+                                                         public boolean onMenuItemClick(MenuItem item) {
+                                                             android.telephony.SmsManager smsManager = SmsManager.getDefault();
+
+                                                             switch (item.getItemId()) {
+                                                                 case R.id.UBC49:
+                                                                     smsManager.sendTextMessage(textTransLink, null, UBC49, null, null);
+                                                                     return true;
+
+                                                                 case R.id.hospital:
+                                                                     smsManager.sendTextMessage(textTransLink, null, hospital, null, null);
+                                                                     return true;
+
+                                                                 case R.id.oak41:
+                                                                     smsManager.sendTextMessage(textTransLink, null, oak41, null, null);
+                                                                     return true;
+
+                                                                 case R.id.oak49:
+                                                                     smsManager.sendTextMessage(textTransLink, null, oak49, null, null);
+                                                                     return true;
+
+                                                                 default:
+                                                                     return true;
+                                                             }
+                                                         }
+                                                     }
+
+                    );
+
+                    popup.show();
+                    return true;
+                } else {
+                    // your code for move and drag
+                    switch (event.getAction())
+
+                    {
+                        case MotionEvent.ACTION_DOWN:
+                            initialX = params.x;
+                            initialY = params.y;
+                            initialTouchX = event.getRawX();
+                            initialTouchY = event.getRawY();
+                            return true;
+                        case MotionEvent.ACTION_UP:
+                            return true;
+                        case MotionEvent.ACTION_MOVE:
+                            params.x = initialX
+                                    + (int) (event.getRawX() - initialTouchX);
+                            params.y = initialY
+                                    + (int) (event.getRawY() - initialTouchY);
+                            windowManager.updateViewLayout(chatHead, params);
+                            return true;
+                    }
+
+                    return false;
+                }
             }
         });
 
@@ -112,5 +187,20 @@ public class TopButton extends Service{
     public void onDestroy() {
         super.onDestroy();
         if (chatHead != null) windowManager.removeView(chatHead);
+    }
+
+    //magic that removes an error, don't touch
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Not used
+        return null;
+    }
+
+    private class SingleTapConfirm extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent event) {
+            return true;
+        }
     }
 }
